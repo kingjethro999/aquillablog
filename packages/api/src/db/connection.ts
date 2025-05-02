20fc4eb8d0a0e5c0586fa96d066c124eaf799268
King Jethro <jethrojerrybj@gmail.com>
2025-05-02 15:45:51 +0100

    prod

--- packages/api/src/db/connection.ts
+++ packages/api/src/db/connection.ts
@@ -2,12 +2,7 @@ import mongoose from 'mongoose';
 
 export const initDb = async (): Promise<any> => {
   try {
-    const db = await mongoose.connect(process.env.MONGO_URL, {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    });
+    const db = await mongoose.connect(process.env.MONGO_URL);
     console.log('DB connected');
     return db;
   } catch (error) {
