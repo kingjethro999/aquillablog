3aea9285367438900dd6d2d7bb369b8846bc4ed7
King Jethro <jethrojerrybj@gmail.com>
2025-05-03 23:06:22 +0100

    prod

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -20,8 +20,10 @@ app.use(compression());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ['https://aquillablog.vercel.app', process.env.FRONTEND_URL],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   })
 );
 
